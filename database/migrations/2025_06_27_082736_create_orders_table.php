1f6695f5ad4901831683ee4e95f3abd856f4810d
yushiro <pc.fyu.1130@gmail.com>
2025-06-27 18:11:56 +0900

    migration完了

---
+++ database/migrations/2025_06_27_082736_create_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
+            $table->enum('payment_method', ['card', 'cash']);
+            $table->string('delivery_name');
+            $table->string('postal_code');
+            $table->string('address');
+            $table->string('building')->nullable();
+            $table->string('phone');
+            $table->enum('status', ['pending', 'completed', 'canceled']);
+            $table->timestamp('created_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
